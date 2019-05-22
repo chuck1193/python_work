934c117b916ab7978a19aa9b4814b96f2fc076aa
chuck1193 <>
2019-05-22 15:22:21 -0500

    Created 'hello, world' and a character calculator in python

---
+++ counting_characters.py
@@ -0,0 +1,12 @@
+def counting_characters():
+
+  words = input("Give me a word and I will tell you the length of that word. ")
+  length = len(words)
+
+  if len(words) == 0:
+    print("You've given me nothing.")
+    counting_characters()
+  else: 
+    print(words + " has " + str(length) + " characters.")
+
+counting_characters()
