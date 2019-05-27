f7a983b3ed685a696cb3f4872f00d0453a371b9d
chuck1193 <>
2019-05-27 13:32:19 -0500

    Simple Math in python

--- simple_math.py
+++ simple_math.py
@@ -1,12 +1,15 @@
-first_number = int(input("What is your first number? "))
-second_number = int(input("What is your second number? "))
+int1 = input("What is your first number? ")
+int2 = input("What is your second number? ")
+
+first_number = int(int1)
+second_number = int(int2)
 
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
 
-print(first_number + "+" + second_number + "=" + sum)
-print(first_number + "-" + second_number + "=" + difference)
-print(first_number + "*" + second_number + "=" + product)
-print(first_number + "/" + second_number + "=" + quotient)
+print("The sum of the 2 numbers is: " + str(sum))
+print("The difference is: " + str(difference))
+print("The product of these two numbers is: " + str(product))
+print("The quotient of these two nhumbers is: " + str(quotient))
