51a7de4d1be4e15307cfa662c196ee7bc228dded
chuck1193 <>
2019-05-29 16:35:00 -0500

    Revised Area of a rectangle room

--- area_of_a_rectangle_room.py
+++ area_of_a_rectangle_room.py
@@ -1,12 +1,20 @@
-int1 = input("What is the length of your room in feet? ")
-int2 = input("What is the width of your room in feet? ")
+def area_of_a_rectangle():
 
-length = int(int1)
-width = int(int2)
+  int1 = input("What is the length of your room in feet? ")
+  int2 = input("What is the width of your room in feet? ")
 
-feet_squared = length * width
-square_meters = feet_squared * 0.09290304
 
-print("You entered the dimensions of " + str(length) + " feet and "  + str(width) + " feet.")
+  if (int1 == '' or int2 == ''):
+    print("You entered nothing, please try again. ")
+    area_of_a_rectangle()
 
-print("The area is " + str(feet_squared) + " square feet. " + str(square_meters) + " square meters.")
+  else:
+    length = int(int1)
+    width = int(int2)
+
+    feet_squared = length * width
+    square_meters = feet_squared * 0.09290304
+    print("You entered the dimensions of " + str(length) + " feet and "  + str(width) + " feet.")
+    print("The area is " + str(feet_squared) + " square feet. " + str(square_meters) + " square meters.")
+
+area_of_a_rectangle()
