ac48149a59dd334f2a577fcfdeb743ba70ba55fe
chuck1193 <>
2019-06-10 00:17:34 -0700

    Sales Tax in Python

---
+++ tax_calculator.py
@@ -0,0 +1,291 @@
+def tax_calculator():
+  int1 = input("What is the order amount?\n")
+  state = input("What is the state?\n")
+
+  order_amount = float(int1)
+  if (state == 'alabama'or  state == 'al'):
+    sales_tax = 4.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'arizona' or state == 'az'):
+    sales_tax = 5.6 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'arkansas' or state == 'ar'):
+    sales_tax = 6.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'california' or state == 'ca'):
+    sales_tax = 7.25 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'colorado' or state == 'co'):
+    sales_tax = 2.9 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'connecticut' or state == 'ct'):
+    sales_tax = 6.35 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'district of columbia' or state == 'dc'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'florida' or state == 'fl'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'georgia' or state == 'ga'):
+    sales_tax = 4.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'hawaii' or state == 'hi'):
+    sales_tax = 4.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'idaho' or state == 'id'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'illinois' or state == 'il'):
+    sales_tax = 6.25 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'indianna' or state == 'in'):
+    sales_tax = 7.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'iowa' or state == 'ia'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'kansas' or state == 'ks'):
+    sales_tax = 6.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'kentucky' or state == 'ky'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'louisiana' or state == 'la'):
+    sales_tax = 4.45 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'maine' or state == 'me'):
+    sales_tax = 5.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'maryland' or state == 'md'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'massachusetts' or state == 'ma'):
+    sales_tax = 6.25 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'michigan' or state == 'mi'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'minnesota' or state == 'mn'):
+    sales_tax = 6.875 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'mississippi' or state == 'ms'):
+    sales_tax = 7.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'missouri' or state == 'mo'):
+    sales_tax = 4.225 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'nebraska' or state == 'ne'):
+    sales_tax = 5.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'nevada' or state == 'nv'):
+    sales_tax = 6.85 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'new jersey' or state == 'nj'):
+    sales_tax = 6.625 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'new mexico' or state == 'nm'):
+    sales_tax = 5.125 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'new york' or state == 'ny'):
+    sales_tax = 4.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'north carolina' or state == 'nc'):
+    sales_tax = 4.75 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'north dakota' or state == 'nd'):
+    sales_tax = 5.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'ohio' or state == 'oh'):
+    sales_tax = 5.75 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'oklahoma' or state == 'ok'):
+    sales_tax = 4.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'pennsylvania' or state == 'pa'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'puerto rico' or state == 'pr'):
+    sales_tax = 11.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'rhode island' or state == 'ri'):
+    sales_tax = 7.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'south carolina' or state == 'sc'):
+    sales_tax = 4.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'south dakota' or state == 'sd'):
+    sales_tax = 4.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'tennessee' or state == 'tn'):
+    sales_tax = 7.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'texas' or state == 'tx'):
+    sales_tax = 6.25 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'utah' or state == 'ut'):
+    sales_tax = 6.25 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total )+ ".")
+  elif (state == 'vermont' or state == 'vt'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'virginia' or state == 'va'):
+    sales_tax = 4.3 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'washington' or state == 'wa'):
+    sales_tax = 6.5 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'west virginia' or state == 'wv'):
+    sales_tax = 6.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'wisconsin' or state == 'wi'):
+    sales_tax = 5.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print("The total is " + str(total) + ".")
+  elif (state == 'wyoming' or state == 'wy'):
+    sales_tax = 4.0 / 100
+    order_tax = order_amount * sales_tax
+    output = order_amount + order_tax
+    total = round(output,2)
+    print( "The total is " + str(total) + ".")
+  else:
+    print( "The total is " + str(order_amount) + ".")
+
+tax_calculator()
