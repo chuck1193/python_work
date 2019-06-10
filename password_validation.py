bc21ea3597118856eec71ca8ccdd047dd81d2b62
chuck1193 <>
2019-06-10 10:13:05 -0700

    Revised Password validation to include a while loop and added in a dictionary so the user can be recognized through the username. Still need to figure out why it's saying that the user is incorrect, even though it is.

--- password_validation.py
+++ password_validation.py
@@ -1,10 +1,51 @@
 import getpass
-def password_validation():
-  password = getpass.getpass("What is the password?\n")
 
-  if (password == "abc$123"):
-    print("Welcome!")
+def password_validation():
+  login = input("Are you already a member?\n")
+
+  if (login == 'no'):
+    username = input("Please give me your username:\n").lower()
+    password = getpass.getpass("Password:\n").lower()
+
+    password_dict = {}
+    password_dict[username] = password
+
+    print(password_dict)
+
+    print("Alright, now please login.\n")
+
+    username = input("What is your username?\n")
+
+    password = getpass.getpass("What is the password?\n")
+
+    if (username in password_dict and password in password_dict):
+      print("Welcome!")
+      exit
+    else:
+      print(password_dict)
+      print("I don't know you. Try Again.")
+      password_validation()
+
   else:
-    print("I don't know you.")
+
+
+    while login == 'yes':
+      username = input("What is your username?\n")
+
+      password = getpass.getpass("What is the password?\n")
+
+      password_dict = {}
+
+      if (KeyError):
+        print("You are not a member. Please create an acoount and then login.\n")
+        password_validation()
+      elif (username == '' or password == ''):
+        print("I don't know you.\n")
+        print("Now we have to start all over. Great.")
+      elif (username == password_dict[username] and password == password_dict[password]):
+        print("Welcome!")
+
+
+  
 
 password_validation()
